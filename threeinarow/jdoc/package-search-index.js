ebc1a22e0df92c7df4c9d6d18e0ad820c272398f
Ritwik Bagga <rbagga@umass.edu>
2020-10-25 14:55:35 -0700

    game complete

--- threeinarow/jdoc/package-search-index.js
+++ threeinarow/jdoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"view"}]
+packageSearchIndex = [{"l":"adaptor"},{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"view"}]
